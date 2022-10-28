1b42a0ed6645c04bc8004a6e0a3ce230e74d46bb
Jacob Brown <dev@jhbforlife.com>
2022-10-27 22:31:09 -0500

    First two handlers completed

--- product-server/main.go
+++ product-server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -48,11 +49,49 @@ func initProducts() {
 }
 
 func GetProductsHandler(w http.ResponseWriter, r *http.Request) {
-
+	log.Println("Received product GET request. Marshaling all product data...")
+	bs, err := json.MarshalIndent(products, "", "    ")
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	log.Println("Marshal successful. Writing JSON...")
+	w.Header().Add("content-type", "application/json")
+	if _, err := w.Write(bs); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	w.WriteHeader(http.StatusOK)
+	log.Println("Write successful.")
 }
 
 func AddProductHandler(w http.ResponseWriter, r *http.Request) {
-
+	var newProduct product
+	log.Println("Received product POST request. Reading incoming JSON...")
+	body, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	log.Println("Read successful. Unmarshaling product...")
+	if err := json.Unmarshal(body, &newProduct); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	log.Println("Unmarshal successful. Adding to existing products...")
+	products = append(products, newProduct)
+	bs, err := json.MarshalIndent(products, "", "    ")
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	if err := os.WriteFile("products.json", bs, 0666); err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	w.WriteHeader(http.StatusOK)
+	log.Println("Successfully added product to list.")
 }
 
 func GetProductIDHandler(w http.ResponseWriter, r *http.Request) {
