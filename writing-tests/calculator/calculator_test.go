03acfdde01c3316670836430b5568f8d0a55527d
Jacob Brown <dev@jhbforlife.com>
2022-11-02 12:28:43 -0500

    Added test file to actually commit logic

--- writing-tests/calculator/calculator_test.go
+++ writing-tests/calculator/calculator_test.go
@@ -3,14 +3,16 @@ package calculator_test
 import (
 	"fmt"
 	"testing"
+
+	"github.com/jhbforlife/pivot-golang/writing-tests/calculator"
 )
 
 func TestCalculator(t *testing.T) {
 	testCases := []struct {
-		a          int
-		b          int
-		operator   string
-		wantResult int
+		a        int
+		b        int
+		operator string
+		want     int
 	}{
 		{123, 321, "+", 444},
 		{900, 99, "+", 999},
@@ -26,13 +28,30 @@ func TestCalculator(t *testing.T) {
 		{100, 0, "/", 0},
 	}
 	for _, tc := range testCases {
-		t.Run(fmt.Sprintf("%d %s %d", tc.a, tc.operator, tc.b), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d%s%d", tc.a, tc.operator, tc.b), func(t *testing.T) {
 			switch tc.operator {
 			case "+":
+				if got := calculator.Add(tc.a, tc.b); got != tc.want {
+					t.Errorf("got: %d - want: %d", got, tc.want)
+				}
 			case "-":
+				if got := calculator.Subtract(tc.a, tc.b); got != tc.want {
+					t.Errorf("got: %d - want: %d", got, tc.want)
+				}
 			case "*":
+				if got := calculator.Multiply(tc.a, tc.b); got != tc.want {
+					t.Errorf("got: %d - want: %d", got, tc.want)
+				}
 			case "/":
+				if got, err := calculator.Divide(tc.a, tc.b); err != nil {
+					if tc.b != 0 {
+						t.Errorf("%d is not 0 but returned error", tc.b)
+					}
+				} else if got != tc.want {
+					t.Errorf("got: %d - want: %d", got, tc.want)
+				}
 			default:
+				t.Errorf("invalid operator: %s", tc.operator)
 			}
 		})
 	}
